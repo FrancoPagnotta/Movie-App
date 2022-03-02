61d9f04059dab11880ddf302c80159a8df7738c6
FrancoPagnotta <francoapagnotta@gmail.com>
2022-03-02 01:10:00 -0300

    Implement swiper slider and movies data

--- src/app/components/slideshow/slideshow.component.ts
+++ src/app/components/slideshow/slideshow.component.ts
@@ -1,17 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { Movie } from 'src/app/interfaces/billboard-response';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-slideshow',
   templateUrl: './slideshow.component.html',
   styleUrls: ['./slideshow.component.css']
 })
-export class SlideshowComponent implements OnInit {
+export class SlideshowComponent implements OnInit, AfterViewInit {
   @Input() inputMovies!: Movie[];
   constructor() { }
 
   ngOnInit(): void {
     console.log(this.inputMovies)
+  }
+
+  ngAfterViewInit(): void {
+    const swiper = new Swiper('.swiper', {
+      // Optional parameters
+      loop: true
+    });
   }
 
 }
