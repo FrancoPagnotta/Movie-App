bd1c154fe16e2f4d4ce87e0c0ab793ac25f424cc
Franco Pagnotta <francoapagnotta@gmail.com>
2022-03-26 00:14:34 -0300

    Import start rating module and pipes module

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,6 +4,8 @@ import { HomeComponent } from './home/home.component';
 import { MovieComponent } from './movie/movie.component';
 import { SearchComponent } from './search/search.component';
 import { ComponentsModule } from '../components/components.module';
+import { RatingModule } from 'ng-starrating';
+import { PipesModule } from '../pipes/pipes.module';
 
 
 
@@ -15,7 +17,9 @@ import { ComponentsModule } from '../components/components.module';
   ],
   imports: [
     CommonModule,
-    ComponentsModule
+    ComponentsModule,
+    RatingModule,
+    PipesModule
   ]
 })
 export class PagesModule { }
