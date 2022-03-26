996b7966571d131065dc37862793dcf63aa1ae63
Franco Pagnotta <francoapagnotta@gmail.com>
2022-03-26 00:15:33 -0300

    Create method that get the found movies and set property

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Movie } from 'src/app/interfaces/billboard-response';
 import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
@@ -9,12 +10,16 @@ import { MoviesService } from 'src/app/services/movies.service';
 })
 export class SearchComponent implements OnInit {
 
+  foundMovies: Movie[] = []
+  movieToSearch!: string
+
   constructor(private activatedRoute: ActivatedRoute,
               private moviesService: MoviesService) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(resp => {
-      this.moviesService.searchMovies(resp.name).subscribe(resp => console.log(resp))
+      this.movieToSearch = resp.name
+      this.moviesService.searchMovies(resp.name).subscribe(resp => this.foundMovies = resp)
     })
   }
 
