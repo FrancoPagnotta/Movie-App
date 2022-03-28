845cf7d8375ed8798913a40713284e6aadb6ac7d
Franco Pagnotta <francoapagnotta@gmail.com>
2022-03-28 15:29:07 -0300

    Implement redirection to the page of the selected movie in the search component

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Movie } from 'src/app/interfaces/billboard-response';
 import { MoviesService } from 'src/app/services/movies.service';
 
@@ -14,13 +14,18 @@ export class SearchComponent implements OnInit {
   movieToSearch!: string
 
   constructor(private activatedRoute: ActivatedRoute,
-              private moviesService: MoviesService) { }
+              private moviesService: MoviesService,
+              private router: Router) {}
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(resp => {
       this.movieToSearch = resp.name
       this.moviesService.searchMovies(resp.name).subscribe(resp => this.foundMovies = resp)
     })
+  }
+
+  viewMovie(movieId: number): void {
+    this.router.navigate(["movie", movieId]);
   }
 
 }
