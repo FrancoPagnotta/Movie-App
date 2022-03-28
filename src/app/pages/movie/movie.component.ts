016b2811f7420032597c36f45ac79f21f7555b62
Franco Pagnotta <francoapagnotta@gmail.com>
2022-03-28 15:31:19 -0300

    Create the template with the data of the searched movie

--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { MovieResponse } from 'src/app/interfaces/movie-response';
 import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
@@ -12,6 +13,7 @@ export class MovieComponent implements OnInit, OnDestroy {
 
   movieId!: number
   subscription!: Subscription
+  movie!: MovieResponse
 
   constructor(private activatedRoute: ActivatedRoute,
               private moviesService: MoviesService) { }
@@ -24,6 +26,7 @@ export class MovieComponent implements OnInit, OnDestroy {
   getDetails(): void {
     this.subscription = this.moviesService.getMovieDetails(this.movieId).subscribe(resp => {
       console.log(resp)
+      this.movie = resp
     })
   }
 
