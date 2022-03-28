8dc63d473f2c3f64ccfcc73ab996e13626767ef4
Franco Pagnotta <francoapagnotta@gmail.com>
2022-03-28 13:07:04 -0300

    Create method that suscribes to getMovieDetails method in movies service

--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
   styleUrls: ['./movie.component.css']
 })
-export class MovieComponent implements OnInit {
+export class MovieComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  movieId!: number
+  subscription!: Subscription
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private moviesService: MoviesService) { }
 
   ngOnInit(): void {
+   this.movieId = this.activatedRoute.snapshot.params.id;
+   this.getDetails()
   }
+
+  getDetails(): void {
+    this.subscription = this.moviesService.getMovieDetails(this.movieId).subscribe(resp => {
+      console.log(resp)
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
+  }
+  
 
 }
