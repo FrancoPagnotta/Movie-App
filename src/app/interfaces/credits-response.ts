a4e980e557cf0543d00b47b70273862ee7ce7091
Franco Pagnotta <francoapagnotta@gmail.com>
2022-03-28 22:15:58 -0300

    Create credits interface

---
+++ src/app/interfaces/credits-response.ts
@@ -0,0 +1,25 @@
+export interface CreditsResponse {
+    id:   number;
+    cast: Cast[];
+}
+
+export interface Cast {
+    adult:                boolean;
+    gender:               number;
+    id:                   number;
+    known_for_department: KnownForDepartment;
+    name:                 string;
+    original_name:        string;
+    popularity:           number;
+    profile_path:         null | string;
+    cast_id:              number;
+    character:            string;
+    credit_id:            string;
+    order:                number;
+}
+
+export enum KnownForDepartment {
+    Acting = "Acting",
+    Art = "Art",
+    Production = "Production",
+}
